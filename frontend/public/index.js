5c0d81fc6ef20beab750f9460facc65ecbde06fa
Wanglem <136154779+WanglemCodes@users.noreply.github.com>
2024-04-07 12:37:40 +0530

    7/4/2024

--- frontend/public/index.js
+++ frontend/public/index.js
@@ -1,10 +1,84 @@
-let movieDivs = document.querySelectorAll('.container .content .movie .slide .movies');
-console.log(movieDivs);
+let movie = document.querySelector('.container .content .movie .slider .slide');
+let container = document.querySelector('.container .content .movie .slider');
 
-/*let movie = document.querySelectorAll('.container .content .movie .slide div');
+console.log(movie);
+console.log(container);
+
+const count = 100;
+let index = 0;
+
+const next = () => {
+   if (index < 40){
+      index += 40;
+      console.log(index);
+      movie.style.transform = `translateX(-${index}%)`
+      
+   }else if (index >= 40){
+      index = 60;
+      console.log(index)
+      movie.style.transform = `translateX(-${index}%)`
+      console.log('tap');
+   }
+}
+const prev = () => {
+   if (index <= 0){
+      index += 150
+      console.log(index);
+      movie.style.transform = `translateX(-${index}%)`
+      
+   }else if (index >= 0){
+      index = 0
+      console.log(index)
+      movie.style.transform = `translateX(-${index}%)`
+      console.log('tap');
+   }
+}
+/*
+  const next = () => {
+    if (count < movieWidth) {
+      count++;
+      movieWidth.style[overflow-x]= `(-${count * movieWidth}px)`;
+    }
+  }
+
+/*
+let movie = document.querySelectorAll('.container .content .movie .slider .slide');
 console.log(movie)
 
+let count = 0;
 
+
+movie.forEach(
+   (slide,index) => {
+      slide.style.left = `${index * 10}%`
+   }
+)
+const next = () => {
+   if (count >= movie.length - 1){
+      count = 0;
+      slider()
+   }else{
+      count++
+      slider()
+   }
+}
+const prev = () => {
+   if (count == 0){
+      count = movie.length-1
+      slider()
+   }else{
+      count--
+   slider()
+   }
+}
+
+const slider = () => {
+   movie.forEach(
+      (slide) => {
+         slide.style.transform = `translateX(-${count*30}%)`
+      }
+   )
+}
 /*
 //let img = document.querySelectorAll('.img');
 //let one = document.getElementById('one');
