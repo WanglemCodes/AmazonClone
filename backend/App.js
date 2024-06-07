3c9936fc0cd719b13124844a5aea2eaaaa281091
Wanglem <136154779+WanglemCodes@users.noreply.github.com>
2024-06-07 14:09:49 +0530

    07/06/2024

--- backend/App.js
+++ backend/App.js
@@ -1,1 +1,39 @@
 const express = require("express");
+const mySql = require("mysql");
+
+const app = express();
+
+const db = mySql.createConnection({
+   host : "localHost",
+   user : "Wang",
+   port : "3100",
+   password : "Wang1234",
+   database : "sakila"
+})
+db.connect((err) => {
+   if (err) {
+       console.error('Error connecting: ' + err.stack);
+       return;
+   }
+   console.log('Connected as id ' + db.threadId);
+});
+
+db.end();
+
+app.get("/wanglem", (req, res) => {
+   console.log("From backend")
+   res.send('Hello from the backend!');
+})
+app.get("/wang", (req, res) => {
+   const q = "SELECT * FROM users";
+   db.query(q, (err, data) => {
+      if (err) {
+         return res.json(err)
+      }
+      return res.json(data)
+   })
+})
+
+app.listen(8000, () => {
+   console.log("Connected")
+})
